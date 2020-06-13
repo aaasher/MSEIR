3f6d2cd3ed6c61df322e75d69b973833ff9cbe04
aaasher <66630600+aaasher@users.noreply.github.com>
2020-06-13 15:43:58 +0300

    Update MSEIR.py

--- MSEIR.py
+++ MSEIR.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# coding: utf-8
+
 import numpy as np
 import pandas as pd
 from scipy.optimize import minimize
@@ -77,7 +80,7 @@ class MSEIR:
         solution = np.asarray(C)
         df_names = ['S', 'E', 'I', 'H', 'R', 'D']
         df_res = pd.DataFrame(solution, columns=df_names)
-        df_res['t'] = range(self.t+1)
+        df_res['t'] = np.linspace(0, self.n_periods, int((self.n_periods)/self.dt)+1)
         df_res['Q'] = self.Q
         df_res['Uf'] = Uf[:len(df_res)]
         df_res['mInf'] = -df_res['S'].diff()
@@ -85,48 +88,76 @@ class MSEIR:
 
         return df_res
     
-    def plot(self, data, since='2020-01-01', size=(700,900), comps='SEIHRD', title='SEIHRD model with optimal control'):
+    def plot(self, data, since='2020-01-01', size=(800,900), comps='SEIHRD', title='SEIHRD model with optimal control'):
         
-        xT = pd.date_range(start = since,
-                           end = datetime.strptime(since, "%Y-%m-%d") + timedelta(days=self.n_periods),
-                           periods=self.n_periods).strftime("%Y-%m-%d")
+        s = datetime.strptime(since, "%Y-%m-%d")
+        xT = data['t'].apply(lambda x: (s + timedelta(x)).strftime("%Y-%m-%d"))
+        
+        control_s = round(100*simps(data['Uf'])/simps(np.ones(len(data['Uf']))),2)
+        infected_s = round(100*(data['D'].max()+data['R'].max())/(data['S'].max()+data['D'].max()+data['R'].max()),2)
+        deceased_s = round(100*(data['D'].max())/(data['D'].max() + data['R'].max()),2)
 
-        fig = make_subplots(rows=3, cols=1, shared_xaxes=True,
+        fig = make_subplots(rows=4, cols=2, shared_xaxes=True,
                             horizontal_spacing=0.01,
                             vertical_spacing=0.01,
-                            row_heights=[0.15, 0.70, 0.15])
+                            row_heights=[0.15, 0.15, 0.50, 0.15],
+                            column_widths=[1.0, 0],
+                           
+                            specs=[[{"type": "Table"}, None],
+                                  [{"type": "scatter"}, None],
+                                  [{"type": "scatter"}, None],
+                                  [{"type": "scatter"}, None]]
+                           )
+                                   
         
         colors = ['#910d00', '#086d71', '#086d71', '#729fcf', '#3465a4',
                   '#086d71', '#cc397b', '#9ca4c4', '#380915', '#1e90ff']
         
         lines = [dict(color=x, width=1.5) for x in colors]
+        
+        fig.add_trace(go.Table(
+                        columnwidth = [50,50],
+                        header=dict(fill_color='white',height=0),
+                        cells=dict(fill_color='white', font=dict(size=13),
+                            values=[['Control Strenght', 'Total Infected (% Population)', 'Total Deceased (% Infected)'],
+                                    [str(control_s)+'%', str(infected_s)+'%', str(deceased_s)+'%']],
+                            align = ['right', 'left'],height=20)), row=1, col=1)
     
-        fig.add_trace(go.Scatter(x=xT, y=data['Uf'], opacity=1.0,name='Uf', line=lines[0]), row=1, col=1)
+        fig.add_trace(go.Scatter(x=xT, y=data['Uf'], opacity=1.0,name='Control (u)', line=lines[0]), row=2, col=1)
         
         if 'S' in comps:
-            fig.add_trace(go.Scatter(x=xT, y=data['S'], opacity=1.0,name='S', line=lines[3]), row=2, col=1)
+            fig.add_trace(go.Scatter(x=xT, y=data['S'], opacity=1.0, visible='legendonly',
+                                     name='Susceptible', line=lines[3]), row=3, col=1)
         if 'E' in comps:
-            fig.add_trace(go.Scatter(x=xT, y=data['E'], opacity=0.8,name='E', line=lines[4]), row=2, col=1)
+            fig.add_trace(go.Scatter(x=xT, y=data['E'], opacity=0.8, visible='legendonly',
+                                     name='Exposed', line=lines[4]), row=3, col=1)
         if 'I' in comps:
-            fig.add_trace(go.Scatter(x=xT, y=data['I'], opacity=0.8,name='I', line=lines[5]), row=2, col=1)
+            fig.add_trace(go.Scatter(x=xT, y=data['I'], opacity=0.8,
+                                     name='Infected', line=lines[5]), row=3, col=1)
         if 'H' in comps:
-            fig.add_trace(go.Scatter(x=xT, y=data['H'], opacity=0.8,name='H', line=lines[6]), row=2, col=1)
+            fig.add_trace(go.Scatter(x=xT, y=data['H'], opacity=0.8,
+                                     name='Hospitalized', line=lines[6]), row=3, col=1)
         if 'R' in comps:
-            fig.add_trace(go.Scatter(x=xT, y=data['R'], opacity=1.0,name='R', line=lines[7]), row=2, col=1)  
+            fig.add_trace(go.Scatter(x=xT, y=data['R'], opacity=1.0, visible='legendonly',
+                                     name='Recovered', line=lines[7]), row=3, col=1)  
         if 'D' in comps:
-            fig.add_trace(go.Scatter(x=xT, y=data['D'], opacity=0.8,name='D', line=lines[8]), row=2, col=1)
+            fig.add_trace(go.Scatter(x=xT, y=data['D'], opacity=0.8,
+                                     name='Deceased', line=lines[8]), row=3, col=1)
         
-        fig.add_trace(go.Scatter(x=xT, y=data['Q'], opacity=1.0, name='Q',
-                                 line=dict(color='#cc397b', width=1.5, dash='dot')), row=2, col=1)
+        fig.add_trace(go.Scatter(x=xT, y=data['Q'], opacity=1.0, name='Healthcare capacity (Q)',
+                                 line=dict(color='#cc397b', width=1.5, dash='dot')), row=3, col=1)
         
-        fig.add_trace(go.Scatter(x=xT, y=data['mInf'], opacity=0.3,name='mInf', line=lines[1]), row=3, col=1)
-        fig.add_trace(go.Scatter(x=xT, y=data['rInf'], opacity=0.6,name='rInf', line=lines[2]), row=3, col=1)
         
-        fig.update_yaxes(title_text="u(t)", row=1, col=1, nticks=6, showgrid=False)
-        fig.update_yaxes(title_text="SEIHRD, Q", row=2, col=1, nticks=6,showgrid=False)
-        fig.update_yaxes(title_text="Inf(t)", row=3, col=1, nticks=6, showgrid=False)
+        fig.add_trace(go.Scatter(x=xT, y=data['mInf'], opacity=0.3,name='Infected (model)', line=lines[1]), row=4, col=1)
+        fig.add_trace(go.Scatter(x=xT, y=data['rInf'], opacity=0.6,name='Infected (real)', line=lines[2]), row=4, col=1)
+        
+        
+        fig.update_yaxes(title_text="Control", row=2, col=1, nticks=4, showgrid=False)
+        fig.update_yaxes(title_text="Compartments", row=3, col=1, nticks=4,showgrid=False)
+        fig.update_yaxes(title_text="Comparison", row=4, col=1, nticks=4, showgrid=False)
         
         fig.update_layout(height=size[0], width=size[1],
-                          legend_orientation="h", legend={'x':0, 'y':1.06, 'itemsizing': 'constant'},
-                          title_text=title)
+                          legend_orientation="h",
+                          legend={'bgcolor': 'rgba(0,0,0,0)', 'itemsizing': 'constant'},
+                          title_x=0.45, title_y=0.9,title_text=title)
         return fig
